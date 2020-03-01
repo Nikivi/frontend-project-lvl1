71bc1250b3f9ead268190f7a8345fd41bf683203
Nikivi <nikivieve@gmail.com>
2020-03-01 20:45:13 +0700

    add lint. Step4

--- Makefile
+++ Makefile
@@ -2,5 +2,8 @@ install:
 	npm install
 start:
 	npx node bin/brain-games.js
+lint:
+	npx eslint .
 publish:
 	npm publish --dry-run
+
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import greeting from '../src/index.js';
 
 greeting();
-
-import greeting from '../src/index.js';
+console.log('Welcome to the Brain Games!');
